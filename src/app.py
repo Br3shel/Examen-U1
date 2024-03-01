e9b8342dc52321d517d66c35b9ab56e6a53204d1
Claudino <brishel_acosta@hotmail.com>
2024-02-29 23:13:17 -0600

    examen finalizado

--- src/app.py
+++ src/app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app=Flask(__name__)
+
 @app.route('/')
 def index():
-    
     return render_template('index.html')
+
+@app.route('/habilidades')
+def habilidades():
+    return render_template('habilidades.html')
+
+@app.route('/datos')
+def datos():
+    return render_template('datos.html')
+
+@app.route('/mensaje', methods=['GET','POST'])
+def mensaje():
+    if request.method=='POST':
+        info=(request.form['mensaje'])
+        return render_template('enviado.html',info=info)
+    else:    
+        return render_template('mensaje.html')
 
 
 if __name__ == '__main__':
